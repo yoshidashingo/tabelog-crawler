4a2e059d21cc472c33c83037825d1699926648ee
tsq <tangshiqiangcn@gmail.com>
2024-02-10 01:36:06 +0800

    Update cron.js

--- script/cron.js
+++ script/cron.js
@@ -1,7 +1,6 @@
 import cron from 'node-cron';
 import start from '../rabbitmq/start.js';
 
-// UTC 16PM = JP 1AM
 cron.schedule('36 2 * * *', () => {
     console.log(new Date().toLocaleString());
     start();
